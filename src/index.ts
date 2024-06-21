105768dcb25157ac64b7ddfba8f33d425b5cdda9
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-06-21 03:34:42 +0100

    code: removed unnecessary importations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv'
 import app from './app'
 import connect from './config/db'
-import employeeModel from './models/Employee'
 
 // dotenv library used to get environment variables
 dotenv.config()
